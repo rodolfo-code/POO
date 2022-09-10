abc45c7d980eaa4172310ffaa5787c2245ba4ff1
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2022-09-10 17:10:34 -0300

    concatenacao e interpolacao

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
---
+++ SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            //Placeholder
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais.", nome, idade, saldo);
+
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo.ToString("F2", CultureInfo.InvariantCulture)} reais" );
+
+            //Concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
+        }
+    }
+}
