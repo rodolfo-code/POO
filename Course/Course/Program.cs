10afe0bf0b462caad4f6ace93313bb467ec9ad6e
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2022-09-09 23:33:05 -0300

    tipos de dados

---
+++ Course/Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483647L;
+            
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
