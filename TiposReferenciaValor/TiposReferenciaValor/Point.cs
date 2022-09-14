0c41728facb9dc139470d0768e619642dd444e7c
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2022-09-13 23:41:30 -0300

    .

---
+++ TiposReferenciaValor/TiposReferenciaValor/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace TiposReferenciaValor
+{
+    internal struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
---
+++ TiposReferenciaValor/TiposReferenciaValor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TiposReferenciaValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+
+            p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
