5d08972d3b8441c84642d938e0617f110845c89d
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2022-09-14 23:14:59 -0300

    vetores co tipo referencia

---
+++ Vetores2/Vetores2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nome do produto");
+                string prod = Console.ReadLine();
+
+                Console.Write("Preço");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Product { Name = prod, Price = preco };
+
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("Media é: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
