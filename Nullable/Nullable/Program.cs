05dd8b64b314637a9a6515480bc3800b75b5669f
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2022-09-14 22:26:15 -0300

    nullable

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<double> x = null;
+
+            //ou
+
+            double? y = 10.00;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+            Console.WriteLine(y.Value);
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+
+            // Operador de coalescência nula
+
+            double? w = null;
+            double? z = 10.00;
+
+            double a = w ?? 5;
+            double b = z ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
