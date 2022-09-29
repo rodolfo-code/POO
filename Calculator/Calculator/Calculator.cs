1c508e0f1a294206fd2365b7caf484c10acba408
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2022-09-29 07:30:45 -0300

    params

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    internal class Calculator
+    {
+        public static int Sum(params int[] Numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                sum += Numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
