ce6a7fe9b6098579d8b2cf0277d17e6d50a6442b
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2022-11-26 12:20:02 -0300

    api sistema tarfas

---
+++ Params/Params/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class Calculator
+    {
+        public static void Triple(int x)
+        {
+            x = x * 3
+        }
+    }
+}
